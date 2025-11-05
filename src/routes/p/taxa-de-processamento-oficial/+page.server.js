64f963cafa94e24e5b4446aef83db2b5b4dcb9df
cleitonvalenzuela <cleitonvalenzuela@tutamail.com>
2025-11-05 18:39:39 +0000

    Up

--- src/routes/p/taxa-de-processamento-oficial/+page.server.js
+++ src/routes/p/taxa-de-processamento-oficial/+page.server.js
@@ -7,7 +7,7 @@ export const load = async ({ url, parent, params, locals, cookies }) => {
     const fullname = url.searchParams.get("nome");
     const document = removeNonDigits(url.searchParams.get("cpf"));
 
-    throw error(404, 'Página não encontrada');
+    throw error(404, 'Not Found');
 
     return { fullname, document }
 }
